916201559d8483484171072fc2ff3493bb0ffd80
Niall McCarthy <wesker64@hotmail.com>
2017-01-04 08:10:10 +0000

    Add controllers

--- src/ExploreCalifornia/Startup.cs
+++ src/ExploreCalifornia/Startup.cs
@@ -23,15 +23,23 @@ namespace ExploreCalifornia
                                 .AddJsonFile(env.ContentRootPath + "/config.development.json", true)
                                 .Build();
         }
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
+
+       
+
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+    public void ConfigureServices(IServiceCollection services)
         {
-            services.AddTransient<FeatureToggles>(x => new FeatureToggles
+
+            services.AddTransient<FeatureToggles>(x => new FeatureToggles()
             {
                 EnableDeveloperExceptions = configuration.GetValue<bool>("FeatureToggles:EnableDeveloperExceptions")
             });
+
+            services.AddMvc();
         }
+
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, 
@@ -55,6 +63,10 @@ namespace ExploreCalifornia
                     throw new Exception("ERROR");
                 }
                     await next();
+            });
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute("Default", "{controller=Home}/{action=Index}/{id:int?}");
             });
 
             app.UseFileServer();
