e68b9a372ef849791c8e020bc0efe8ccef0288c4
Napper64 <Wesker64@Hotmail.com>
2017-02-28 23:03:37 +0000

    Implementing TagHelpers and strongly typed forms (Razor)

--- src/ExploreCalifornia/Controllers/BlogController.cs
+++ src/ExploreCalifornia/Controllers/BlogController.cs
@@ -12,34 +12,57 @@ namespace ExploreCalifornia.Controllers {
 
     [Route("Blog")]
     public class BlogController : Controller {
-        public IActionResult Index() {
+        public IActionResult Index()
+        {
 
             IList<Post> posts = new List<Post>();
-            posts.Add(new Post {
+            posts.Add(new Post
+            {
                 Title = "My blog post",
                 Posted = DateTime.Now,
                 Author = "Niall McCarthy",
                 Body = "This is a blog post",
             });
-            posts.Add(new Post {
+            posts.Add(new Post
+            {
                 Title = "My second blog post",
                 Posted = DateTime.Now,
                 Author = "Niall McCarthy",
                 Body = "This is another blog post",
             });
             return View(posts);
-    }
+        }
 
-    [Route("{year:min(2000)}/{month:range(1,12)}/{key}")]
-    public IActionResult Post(int year, int month, string key) {
-        Post post = new Post {
-            Title = "My blog post",
-            Posted = DateTime.Now,
-            Author = "Niall McCarthy",
-            Body = "This is the body",
-        };
+        [Route("{year:min(2000)}/{month:range(1,12)}/{key}")]
+        public IActionResult Post(int year, int month, string key)
+        {
+            Post post = new Post
+            {
+                Title = "My blog post",
+                Posted = DateTime.Now,
+                Author = "Niall McCarthy",
+                Body = "This is the body",
+            };
 
-        return View(post);
+            return View(post);
+        }
+
+        [HttpGet, Route("create")]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost, Route("create")]
+        public IActionResult Create(Post post)
+        {
+            if (!ModelState.IsValid)
+                return View();
+
+            post.Author = User.Identity.Name;
+            post.Posted = DateTime.Now;
+            return View();
+        }
+
     }
-}
 }
--- src/ExploreCalifornia/Models/Post.cs
+++ src/ExploreCalifornia/Models/Post.cs
@@ -1,27 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace ExploreCalifornia.Models {
 
     public class Post {
-        public string Title {
-            get;
-            set;
-        }
 
-        public string Author {
-            get;
-            set;
-        }
-        public string Body {
-            get;
-            set;
-        }
-        public DateTime Posted {
-            get;
-            set;
-        }
+        [Display(Name = "Post Title")]
+        [Required]
+        [DataType(DataType.Text)]
+        [StringLength(100, MinimumLength = 5, ErrorMessage = "Title must be betwen 5 and 100 characters long")]
+        public string Title { get; set; }
+
+        public string Author { get; set; }
+
+        [Required]
+        [DataType(DataType.MultilineText)]
+        [StringLength(100, ErrorMessage = "Blog posts must be at least 100 characters long")]
+        public string Body { get; set; }
+
+        public DateTime Posted { get; set; }
     }
+
 }
